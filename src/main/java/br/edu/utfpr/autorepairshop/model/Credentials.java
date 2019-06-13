0248568dc8b41d195b6c02977a204997d8fb27e0
BrunaBelo <anabruna28@gmail.com>
2019-06-13 07:51:05 -0300

    Alteração atributo id da entidade credentials

--- src/main/java/br/edu/utfpr/autorepairshop/model/Credentials.java
+++ src/main/java/br/edu/utfpr/autorepairshop/model/Credentials.java
@@ -1,6 +1,10 @@
 package br.edu.utfpr.autorepairshop.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -15,12 +19,14 @@ import lombok.Setter;
 @NoArgsConstructor
 @Data
 public class Credentials {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_credentials")
 	private Long idCredentials;
 	private String email;
 	private String password;
 
 	public Credentials(Long idCredentials, String email, String password) {
-		super();
 		this.idCredentials = idCredentials;
 		this.email = email;
 		this.password = password;
