c40556488aadc7d05af45b05a8e3598ed85f15ab
samuel <samboeira@gmail.com>
2019-07-10 10:53:33 -0300

    Correção nome da classe errado

--- src/main/java/br/edu/utfpr/autorepairshop/model/repository/AddressRepository.java
+++ src/main/java/br/edu/utfpr/autorepairshop/model/repository/AddressRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.utfpr.autorepairshop.model.Address;
 
-public interface AddressRpository extends JpaRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
 
 }
