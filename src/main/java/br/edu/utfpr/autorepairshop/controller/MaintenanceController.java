25b558acd28eddddeeade28bf8d63ec710ca9cef
samuel <samboeira@gmail.com>
2019-07-12 13:07:10 -0300

    commit

--- src/main/java/br/edu/utfpr/autorepairshop/controller/MaintenanceController.java
+++ src/main/java/br/edu/utfpr/autorepairshop/controller/MaintenanceController.java
@@ -20,10 +20,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -130,7 +127,7 @@ public class MaintenanceController {
 //    }
 
     @GetMapping(value = "pesquisa")
-    public ModelAndView search(@PathVariable("search") String placa) {
+    public ModelAndView search(@RequestParam(value = "search", defaultValue = "") String placa) {
         return new ModelAndView();
     }
 
